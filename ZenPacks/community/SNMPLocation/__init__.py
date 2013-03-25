6132b0bf96a71e52541612da046c4502e66b4142
Daniel Robbins <drobbins@funtoo.org>
2013-03-25 20:21:58 +0300

    2.0 release beta. Needs testing.

--- ZenPacks/community/SNMPLocation/__init__.py
+++ ZenPacks/community/SNMPLocation/__init__.py
@@ -6,19 +6,47 @@ from Products.ZenModel.ZenPack import ZenPack as ZenPackBase
 
 unused(Globals)
 
-# SNMPLocationCallback() is sent location data from the modeler plugin and can
-# perform actions to update the device. Common operations would involve putting
-# the device in an organizer based on the name of the SNMP location.
-#
-# Other actions could also be performed, such as initialization of the device's
-# rackSlot value... or anything else.
+# To use the SNMP Location data to update the device, we need *two* methods.
+# They are setSNMPLocation() and getSNMPLocation(), and they must begin with
+# the "get" and "set" prefix. Monkey patching is used to add them to the
+# base Device object in Zenoss, which is the right thing to do because we
+# want this functionality enabled for *all* devices in Zenoss.
+
+# This code works in conjunction with the collector/modeler plugin defined
+# in modeler/plugins/community/snmp/LocationMap.py. The two methods below
+# need to exist for the modeler plugin to update the device objects properly.
+
+# Here is how these two methods work. Our modeler plugin will get a new
+# SNMP location value, and it will place this value in an objectMap.
+
+# Zenoss will compare the value placed in the objectMap with the return value
+# of getSNMPLocation() call.
+ 
+# Zenoss will see if the values match. If they do, no action will be taken as
+# the object will be considered to already have this value applied.
+
+# However, if the values don't match, then device.setSNMPLocation(newvalue)
+# will be called which will take care of updating the object.
 
 @monkeypatch("Products.ZenModel.Device.Device")
-def SNMPLocationCallback(self, location):
+def setSNMPLocation(self, location):
     self.setLocation(location)
     
-    if self.rackSlot == "":
-        self.rackSlot = "nifty"
+@monkeypatch("Products.ZenModel.Device.Device")
+def getSNMPLocation(self):
+    
+    loc = self.getLocationName()
+
+    # getLocationName() will return an empty string if the device isn't in
+    # a location. Otherwise, it will return the fully-qualified location,
+    # with each organizer separated by a "/". The location, if set, will 
+    # also have an initial "/".
+
+    # The collector plugin takes care of "tweaking" the location value
+    # receieved from SNMP so that it is Zenoss-friendly (no illegal characters)
+    # and will match the format returned by getLocationName().
+
+    return loc
 
 # Below, we create a sub-class of the ZenPack object which will be used to 
 # install and remove this ZenPack. This allows us to automatically add our
@@ -31,48 +59,46 @@ class ZenPack(ZenPackBase):
 
     modeler_plugins = [ 'community.snmp.LocationMap' ]
 
-    # Iterator of device classes and devices on which we will auto-enable and auto-
-    # disable on modeler plugin on install/removal of this ZenPack.
+    def devices_or_classes_to_modify_iterator(self, app):
+    
+    # This method returns an iterator of device classes and devices on which we
+    # will auto-enable and auto- disable on modeler plugin on install/removal
+    # of this ZenPack.
 
     # This method returns the root device class plus all devices and device classes
     # with overridden modeler plugin settings. This has the effect of enabling/
     # disabling it globally:
 
-    def devices_or_classes_to_modify_iterator(self):
         yield app.zport.dmd.Devices.getOrganizer('/')
         for x in app.zport.dmd.Devices.getOverriddenObjects('zCollectorPlugins'):
             yield x
 
-    # Add a modeler plugin to a particular device or device class:
-
     def add_plugin_to_device_or_class(self, d, plugin):
+        """Add a modeler plugin to a particular device or device class."""
         plugins = list(d.zCollectorPlugins)
         if plugin not in plugins:
             log.info('Adding %s modeler plugin to %s', plugin, d.id)
             plugins.append(plugin)
             d.setZenProperty('zCollectorPlugins', plugins)
 
-    # Remove a modeler plugin from a particular device or device class:
-
     def remove_plugin_from_device_or_class(self, d, plugin):
+        """Remove a modeler plugin from a particular device or device class"""
         plugins = list(d.zCollectorPlugins)
         if plugin in plugins:
             log.info('Removing %s modeler plugin from %s', plugin, d.id)
             plugins.remove(plugin)
             dc.setZenProperty('zCollectorPlugins', plugins)
 
-    # Enable modeler plugins (main method)
-
     def enable_modeler_plugins(self, dmd, myplugin_iter, mydev_iter):
+        """Enable modeler plugin(s) (main method)"""
         for plugin in myplugin_iter:
             for d in mydev_iter:
                 # Only add if SNMP monitoring is enabled:
                 if not d.zSnmpMonitorIgnore:
                     self.add_plugin_to_device_or_class(d, plugin)
-   
-    # Disable modeler plugins (main method)
  
     def disable_modeler_plugins(self, dmd, myplugin_iter, mydev_iter):
+        """Disable modeler plugin(s) (main method)"""
         for plugin in myplugin_iter:
             for d in mydev_iter:
                 try:
@@ -85,10 +111,12 @@ class ZenPack(ZenPackBase):
 
     def install(self, app):
         ZenPackBase.install(self, app)
-        self.enable_modeler_plugins( app.zport.dmd, self.modeler_plugins, self.devices_or_classes_to_modify_iterator() )
+        # enable globally, for all device classes and devices:
+        self.enable_modeler_plugins( app.zport.dmd, self.modeler_plugins, self.devices_or_classes_to_modify_iterator(app) )
 
     def remove(self, app, leaveObjects=False):
-        self.remove_modeler_plugins(app.zport.dmd, self.modeler_plugins, self.devices_or_classes_to_modify_iterator() )
+        # disable globally, for all device classes and devices:
+        self.remove_modeler_plugins(app.zport.dmd, self.modeler_plugins, self.devices_or_classes_to_modify_iterator(app) )
         ZenPackBase.remove(self, app, leaveObjects=leaveObjects)
 
 # vim: set ts=4 sw=4 expandtab: 
--- ZenPacks/community/SNMPLocation/modeler/plugins/community/snmp/LocationMap.py
+++ ZenPacks/community/SNMPLocation/modeler/plugins/community/snmp/LocationMap.py
@@ -4,17 +4,52 @@ class LocationMap(SnmpPlugin):
     
     maptype = "LocationMap"
 
-    snmpGetMap = GetMap({
-            '.1.3.6.1.2.1.1.6.0' : 'SNMPLocationCallback',
-            })
+    snmpGetMap = GetMap({ '.1.3.6.1.2.1.1.6.0' : 'setSNMPLocation' })
     
     def process(self, device, results, log):
+
         """Collect SNMP location information from this device"""
+
         log.info('processing %s for device %s', self.name(), device.id)
+
         getdata, tabledata = results
         om = self.objectMap(getdata)
-        om.setLocationviaSNMP = '/%s' % (self.prepId(om.setLocationviaSNMP))
+
+        # Now it's time to place our SNMP location value inside an objectMap.
+        # Zenoss will use this objectMap to "imprint" any necessary value
+        # changes to the underlying device object.  Specifically, our new SNMP
+        # value will interface with our getSNMPLocation() and setSNMPLocation()
+        # methods on the underlying device. 
+
+        # We won't hand back the raw location value received from SNMP,
+        # however. Instead, we will tweak the value somehwat:
         
-        log.info('Location: %s', om.setLocationviaSNMP)
+        # prepId() will be used to convert illegal device/organizer characters
+        # to underscores. But we will preserve any '/' characters (also
+        # considered illegal in location organizer names) since our methods use
+        # them as a means to specify a heirarchy of location organizers.
+
+        # We will also prepend a "/" to the string if it not blank and doesn't
+        # begin with a ".".
+        
+        # These changes will prepare the SNMP location value for acceptance by
+        # our setSNMPLocation() method, and also ensure that if the SNMP value
+        # has already been applied, that our tweaked SNMP string will match
+        # that returned by getSNMPLocation(), to avoid unnecessary model
+        # updates.
+
+        newval = om.setSNMPLocation        
+        if len(newval):
+            # run prepId() on all parts between slashes:
+            newval = "/".join(map(self.prepId, newval.split("/"))) 
+            # likely, an initial "/" was not in the SNMP location. Check. Add if needed.
+            if newval[0] != "/":
+                newval = "/" + newval
+            # update our objectMap with our tweaked value:
+            om.setSNMPLocation = newval
+
+        log.info('Location: %s', om.setSNMPLocation)
+
         return om
 
+# vim: set ts=4 sw=4 expandtab: 
